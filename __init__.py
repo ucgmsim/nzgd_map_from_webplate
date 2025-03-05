666dad51d50144c538d6bfe7f42a965b5fa04352
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2025-03-05 14:14:13 +1300

    fixing inits

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# nzgd_map/__init__.py
-from .app import create_app
-
-# Create and expose the Flask application
-app = create_app()
---
+++ nzgd_map/__init__.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Any
+
+from flask import Flask
+
+
+def create_app(test_config: Any = None):
+    """Build a flask app for serving."""
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app_path = Path(app.instance_path)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # import our views and register them with the app.
+    from app import views
+
+    app.register_blueprint(views.bp)
+
+    # ensure the instance folder exists
+    app_path.mkdir(exist_ok=True)
+
+    return app
